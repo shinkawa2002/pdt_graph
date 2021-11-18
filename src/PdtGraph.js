1c2864e0679a9bcbc5b61a6188b47e3db3bb7482
Shinichi Kawakami <shinichi.kawakami@looker.com>
2021-11-18 05:37:06 +0000

    Change to use DT Graph for View

--- src/PdtGraph.js
+++ src/PdtGraph.js
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React, { useEffect, useState, useContext, useReducer } from 'react'
-import { Space, ComponentsProvider, Span, FieldSelect, MessageBar, SpaceVertical } from '@looker/components'
+import React, { useEffect, useState, useContext, useCallback } from 'react'
+import { Space, ComponentsProvider, Span, FieldSelect, MessageBar, SpaceVertical, Spinner, ProgressCircular } from '@looker/components'
 import { ExtensionContext } from '@looker/extension-sdk-react'
 import Graphviz from 'graphviz-react'
 
@@ -24,12 +24,19 @@ import Graphviz from 'graphviz-react'
 export const PdtGraph = () => {
   const { core40SDK } = useContext(ExtensionContext)
   const [message, setMessage] = useState()
-  const [desciption, setDescription] = useState('モデルを選択してください')
   const [lookmlModels, setLooklModels] = useState([])
+  const [modelOptions, setModelOptions] = useState([])
   const [selectedModel, setSelectedModel] = useState()
   const [graphText, setGraphText] = useState()
   const [windowDims, setWindowDims] = useState({})
   const [isModelLoading, setIsModelLoading] = useState(true)
+  const [isConnLoading, setIsConnLoading] = useState(true)
+  const [isPdtLoading, setIsPdtLoading] = useState(false)
+  const [pdtViews, setPdtViews] = useState([])
+  const [connections, setConnections] = useState([])
+  const [model, setModel] = useState()
+  const [connection, setConnection] = useState()
+  const [isGraphLoading, setIsGraphLoading] = useState(false)
 
   const getWindowDimensions = () => {
     const { innerWidth: width, innerHeight: height } = window;
@@ -49,50 +56,192 @@ export const PdtGraph = () => {
   useEffect(() => {
     const initialize = async () => {
       try {
-        const models = await core40SDK.ok(core40SDK.all_lookml_models('label,name,project_name'))
+        const result = await core40SDK.ok(core40SDK.all_connections('name'))
+        const models = []
+        const cons = []
+        if (result) {
+          const cons = result.map((rec) => {
+            return rec['name']
+          })
+
+          setConnections(cons)
+          setIsConnLoading(false)
+        }
+      } catch (error) {
+        setMessage('Error occured getting connections')
+        console.error(error)
+      }
+
+      try {
+        const models = await core40SDK.ok(core40SDK.all_lookml_models('label,name,project_name,allowed_db_connection_names'))
         setLooklModels(models)
+        updateModelOptions()
         setIsModelLoading(false)
       } catch (error) {
         setMessage('Error occured getting model information')
         console.error(error)
-      }  
+      }
+
     }
     initialize()
   },[])
 
-  // get derive table graph for model
-  const getModelGraph = async (model) => {
+  useEffect(() => {
+    const getPdts = async () => {
+      setIsPdtLoading(true)
+      try {
+        const filterOps = {}
+        if (model) {
+          filterOps["pdt_builds.model_name"] = model
+        }
+        if (connection) {
+          filterOps["pdt_builds.connection"] = connection
+        }
+        const result = await core40SDK.ok(core40SDK.run_inline_query(
+          {
+            result_format: 'json',
+            body: {
+              model: 'system__activity',
+              view: 'pdt_builds',
+              fields: [
+                'pdt_builds.connection',
+                'pdt_builds.model_name',
+                'pdt_builds.view_name'
+              ],
+              filters: filterOps,
+              total: false,
+              runtime: 0
+            },
+            limit: 500
+          }))
+        if (result) {
+          const pdtData = result.map((record) => {
+            return {
+              connection: record['pdt_builds.connection'],
+              model: record['pdt_builds.model_name'],
+              view: record['pdt_builds.view_name']
+            }
+          })
+          setPdtViews(pdtData)
+        } else {
+          setPdtViews([])
+        }
+
+      } catch (error) {
+        setMessage('Error occured getting PDTs')
+        console.error(error)
+      }
+      setIsPdtLoading(false)
+    }
+    
+    if (!model || !connection) {
+      return
+    } else {
+      getPdts()
+    }
+  },[model,connection])
+
+  const getPdtGraph = useCallback(async (selectPdt, model) => {
+    setIsGraphLoading(true)
     try {
-      setGraphText('')
-      const result = await core40SDK.ok(core40SDK.graph_derived_tables_for_model({model: model}))
-      if (result.graph_text.indexOf("subgraph") > -1) {
+      const result = await core40SDK.ok(core40SDK.graph_derived_tables_for_view({
+        view: selectPdt,
+        models: model
+      }))
+      if (result) {
         setGraphText(result.graph_text)
       } else {
-        setDescription('PDTが存在しません。他のモデルを選択してください。')
+        setGraphText("")
       }
     } catch (error) {
-      setMessage('Error occured getting PDT graph')
+      setMessage("Error occured getting pdt graph")
       console.error(error)
     }
-  }  
+    setIsGraphLoading(false)
+  },[])
 
   // create model option list
-  const modelOptions = lookmlModels.map(({label, name}) => {
+  const updateModelOptions = (con) => {
+    const options = []
+    const _con = con ? con : connection
+    lookmlModels.forEach((item) => {
+      if (_con && _con !== "") {
+        const cons = item['allowed_db_connection_names']
+        if (cons.includes(_con)) {
+          options.push({
+            value: item['name'],
+            label: item['label']            
+          })
+        }
+      }
+    })
+    setModelOptions(options)
+  }
+  
+  const connOptions = connections.map((item) => {
     return {
-      value: name,
-      label: label
+      value: item,
+      label: item
+    }
+  })
+
+  const pdtOpsions = pdtViews.map((item) => {
+    return {
+      value: item.view,
+      label: item.view
     }
   })
 
   const modelSelected = (value) => {
     setSelectedModel(value)
-    getModelGraph(value)
+    setModel(value)
+    // getModelGraph(value)
+  }
+
+  const conSelected = (value) => {
+    setConnection(value)
+    setSelectedModel("")
+    updateModelOptions(value)
+  }
+
+  const pdtSelected = (value) => {
+    getPdtGraph(value, model)
   }
 
   const graphOptions = {
     fit: true,
     height: windowDims.height * 0.8,
     width: windowDims.width
+  }
+
+  const StatusMessage = () => {
+    let isLoading = false
+    let desc = ""
+    if (isPdtLoading || isModelLoading || isConnLoading) {
+      isLoading = true
+    }
+
+    if (isConnLoading && isModelLoading) {
+      desc = '初期設定中'
+    } else if (isModelLoading) {
+      desc = 'モデル設定中'
+    } else if (isPdtLoading) {
+      desc = 'PDT取得中'
+    } else if (!graphText || graphText === "") {
+      desc = 'PDTを選択してください'
+    } else {
+      desc = 'コネクション、モデル、PDTを選択してください'
+    }
+
+    return (
+    <>
+      {desc !== "" && (
+        <Span fontSize="large">
+        {desc}{isLoading && (<Spinner/>)}
+        </Span>
+      )}
+    </>
+    )
   }
 
   return (
@@ -103,21 +252,43 @@ export const PdtGraph = () => {
             {message && (
             <MessageBar intent="critical" >{message}</MessageBar>
             )}
+            <Space>
             <FieldSelect
-            key="model_selector"
-            name="model_selector"
-            label="Model"
-            options={modelOptions}
-            onChange={modelSelected}
-            default={selectedModel}
-            isLoading={isModelLoading}
-          />
-          {!graphText && (
-            <Span fontSize="large">
-            {desciption}
-          </Span>
+              key="connection_selector"
+              name="connection_selector"
+              label="Connection"
+              options={connOptions}
+              isLoading={isConnLoading}
+              onChange={conSelected}
+              disabled={isPdtLoading}
+            />
+            <FieldSelect
+              key="model_selector"
+              name="model_selector"
+              label="Model"
+              options={modelOptions}
+              onChange={modelSelected}
+              default={selectedModel}
+              isLoading={isModelLoading}
+              disabled={isPdtLoading}
+            />
+            <FieldSelect
+              key="pdt_selector"
+              name="pdt_selector"
+              label="PDT"
+              options={pdtOpsions}
+              isLoading={isPdtLoading}
+              onChange={pdtSelected}
+              disabled={isGraphLoading}
+            />
+          </Space>
+          <StatusMessage/>
+          {isGraphLoading && (
+            <Space justifyContent="center">
+              <ProgressCircular/>
+            </Space>
           )}
-          {graphText && (
+          {!isGraphLoading && graphText && graphText !== "" && (
             <Graphviz
               dot={graphText}
               options={graphOptions}
